bc6b0161f0b33ce2ac6ef24d55d5071c2efd5694
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-06-09 17:08:53 +0400

    update

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
+terraform {
+  required_version = ">= 0.10.3" # introduction of Local Values configuration language feature
+}
+
 module "label" {
   source      = "git::https://github.com/terraform-library/terraform-label.git?ref=master"
   name        = "${var.name}"
   environment = "${var.environment}"
   managedby   = "${var.managedby}"
   delimiter   = "${var.delimiter}"
-}
-
-terraform {
-  required_version = ">= 0.10.3" # introduction of Local Values configuration language feature
 }
 
 ######
